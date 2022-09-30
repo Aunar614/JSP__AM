a1b87264be2c0dcbf1ff2e94512c8fdd7fc1102a
Aunar614 <developkim614@gmail.com>
2022-09-30 19:19:35 +0900

    디스패쳐 서블릿 도입 중

--- src/main/java/com/KoreaIT/java/am/servlet/DispatcherServlet.java
+++ src/main/java/com/KoreaIT/java/am/servlet/DispatcherServlet.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 
 import com.KoreaIT.java.am.config.Config;
+import com.KoreaIT.java.am.controller.ArticleController;
 import com.KoreaIT.java.am.exception.SQLErrorException;
 import com.KoreaIT.java.am.util.DBUtil;
 import com.KoreaIT.java.am.util.SecSql;
@@ -19,17 +19,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/article/list")
-public class ArticleListServlet extends HttpServlet {
-
+@WebServlet("/s/*")
+public class DispatcherServlet extends HttpServlet {
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		response.setContentType("text/html; charset=UTF-8");
-
+		request.setCharacterEncoding("UTF-8");
+		
 		// DB 연결
-
+		
 		Connection conn = null;
 
 		String driverName = Config.getDBDriverClassName();
@@ -46,8 +47,46 @@ public class ArticleListServlet extends HttpServlet {
 		try {
 			conn = DriverManager.getConnection(Config.getDBUrl(), Config.getDBUser(), Config.getDBPassword());
 			
-
-
+			// 모든 요청에 응답하기 전에 무조건 해야함
+			HttpSession session = request.getSession();
+			
+			boolean isLogined = false;
+			int loginedMemberId = -1;
+			Map<String, Object> loginedMemberRow = null;
+			
+			if (session.getAttribute("loginedMemberLoginId") != null) {
+				loginedMemberId = (int)session.getAttribute("loginedMemberId");
+				isLogined = true;
+				
+				SecSql sql = SecSql.from("SELECT * FROM `member`");
+				sql.append("WHERE id = ?;", loginedMemberId);
+				loginedMemberRow = DBUtil.selectRow(conn, sql);
+				
+			}
+			
+			request.setAttribute("isLogined", isLogined);
+			request.setAttribute("loginedMemberId", loginedMemberId);
+			request.setAttribute("loginedMemberRow", loginedMemberRow);
+			
+			String requestUri = request.getRequestURI();
+			String[] requestUriBits = requestUri.split("/");
+			// [0]/s [1]/article [2]/list
+			
+			if (requestUriBits.length < 5) {
+				response.getWriter().append("올바른 요청이 아닙니다");
+				return;
+			}
+			
+			String controllerName = requestUriBits[3];
+			String actionMethodName = requestUriBits[4];
+			
+			if (controllerName.equals("article")) {
+				ArticleController articleController = new ArticleController(request, response, conn);
+				
+				if(actionMethodName.equals("list")) {
+					articleController.showList();
+				}
+			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
